d5eebf625cfbd3509d20b90827fdfb68f162daee
HMXHKAKA <dreamkaka457@gmail.com>
2023-05-21 18:44:42 +0800

    next-update

---
+++ theme.config.jsx
@@ -0,0 +1,15 @@
+export default {
+    logo: <span>晓寒的无聊逼逼站</span>,
+    project: {
+      link: 'https://github.com/Dreamkaka'
+    
+    },
+    footer: {
+        text: '愿此行，终抵群星-----©2023 幻梦晓寒',
+      },
+      useNextSeoProps() {
+        return {
+          titleTemplate: '%s – XIAOHAN-KAKA'
+        }
+      }
+    } 
